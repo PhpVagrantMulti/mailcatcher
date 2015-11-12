a21a517cf0bdd9e3c862c2b8c9bfc07ca05f899f
Seth Griffin <griffinseth@yahoo.com>
2015-11-11 20:30:00 -0600

    Updated version info

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'phpvagrantmulti@gmail.com'
 license          'GPL v3'
 description      'Installs/Configures mailcatcher for PhpVagrantMulti'
 long_description 'Installs/Configures mailcatcher for PhpVagrantMulti'
-version          '0.1.0'
+version          '0.1.1'
 
